b622ccae0c67f319e7af186f994ac3c9bc05d793
yousef-alhabbash <yousef.al-habbash@mths.ca>
2024-09-23 17:28:03 -0400

    Basic hello world

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Mr. Yousef
+ * Created on: Sep 2024
  * This program ...
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, Yousef!')
