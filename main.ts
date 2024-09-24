922aa83b4b812ed0ee39408245ffda740c0856b7
yousef-alhabbash <yousef.al-habbash@mths.ca>
2024-09-24 17:08:44 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
  *
  * Created by: Mr. Yousef
  * Created on: Sep 2024
- * This program is made to say hellow word.
+ * This program is made to say hello word.
 */
 
 basic.showString('Hello, Yousef!')
